1b100c30edd2ec674b241198ad8f715984723e8c
satyapraneet63 <satyapraneet63@gmail.com>
2026-07-28 13:07:53 +0530

    Add LaTeX support (vimtex + texlab)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Use zathura instead of the OS-default PDF viewer (okular) for vimtex's :VimtexView
+vim.g.vimtex_view_method = "zathura"
